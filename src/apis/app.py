01cf7b2b59d0d4d43acd75a3e132bcdbd1060d95
Carlos Andres Osorio Alcalde <CarlosAOsorio@comfama.com.co>
2023-01-30 10:00:43 -0500

    Delete unnecessary files from mec_points in python

--- src/apis/app.py
+++ src/apis/app.py
@@ -10,23 +10,14 @@ from fastapi.encoders import jsonable_encoder
 from src.apis.get_config import *
 from typing import Dict
 from pydantic import BaseModel
-import wx
 
 # Create the app
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-def convert_string_perc_to_float(string : str) -> float:
-    return float(string.replace('%', '')) / 100
-
 def send_data_random_game(n_figures : int = 3, different_lens : bool = True, different_rotation : bool = True) -> Dict:
-    canvas_x_perc_size, canvas_y_perc_size = canvas_size()
-    app_cx = wx.App(False)
-    display_size_x, display_size_y = wx.GetDisplaySize()
-
-    canvas_x_size = int(convert_string_perc_to_float(canvas_x_perc_size) * display_size_x)
-    canvas_y_size = int(convert_string_perc_to_float(canvas_y_perc_size) * display_size_y)
+    canvas_x_size, canvas_y_size = canvas_size()
 
     game_setup, difficulty = create_random_setup(
                                                 canvas_x_size, 
@@ -80,15 +71,3 @@ async def render_game(request: Request, n_figures : int):
     response_data = send_data_random_game(n_figures = n_figures)
     response_data['request'] = request
     return templates.TemplateResponse("index.html", response_data)
-
-
-if __name__ == "__main__":
-    import uvicorn
-    data = get_config_data()
-    uvicorn.run(
-                "src.apis.app:app", 
-                host="0.0.0.0", 
-                port=data['port'], 
-                reload=True,
-                reload_dirs=['src', 'static/js', 'static/css', 'templates']
-    )
--- src/apis/get_config.py
+++ src/apis/get_config.py
@@ -1,4 +1,5 @@
 import yaml
+import wx
 
 # Get the data of config data
 def get_config_data():
@@ -6,8 +7,18 @@ def get_config_data():
         data = yaml.safe_load(f)
     return data
 
+def convert_string_perc_to_float(string : str) -> float:
+    return float(string.replace('%', '')) / 100
+
 def canvas_size():
     canvas_size = get_config_data()['canvas_size']
-    canvas_x_size = canvas_size[0]['x_size']
-    canvas_y_size = canvas_size[1]['y_size']
+    canvas_x_perc_size = canvas_size[0]['x_size']
+    canvas_y_perc_size = canvas_size[1]['y_size']
+
+    app_cx = wx.App(False)
+    display_size_x, display_size_y = wx.GetDisplaySize()
+
+    canvas_x_size = int(convert_string_perc_to_float(canvas_x_perc_size) * display_size_x)
+    canvas_y_size = int(convert_string_perc_to_float(canvas_y_perc_size) * display_size_y)
+
     return canvas_x_size, canvas_y_size
