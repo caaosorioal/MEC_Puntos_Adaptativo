bf9d55a42f0e165996db2970ca1222d45538af91
Carlos Andrés Osorio <caaosorioal@unal.edu.co>
2023-01-30 23:01:10 -0500

    Update requirements

--- static/js/join_points.js
+++ static/js/join_points.js
@@ -2,6 +2,8 @@
 var server_port = window.location.port || 8000;
 var server_host = window.location.host || '0.0.0.0';
 
+console.log(server_port, server_host);
+
 // Render the number of squares and triangles
 $(window).load(
     function(){
