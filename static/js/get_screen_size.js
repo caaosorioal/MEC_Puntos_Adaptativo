8782c39a41b9e46453aaea2f6b808325e870a427
Carlos Andrés Osorio <caaosorioal@unal.edu.co>
2023-01-30 23:07:55 -0500

    Update requirements

--- static/js/get_screen_size.js
+++ static/js/get_screen_size.js
@@ -3,4 +3,4 @@
 var height_size = $(document).height();
 var width_size = $(document).width();
 
-connection_to_server({'x_size': width_size , 'y_size': height_size}, 'POST', `0.0.0.0:${server_port}/get-size-canvas/`);
+connection_to_server({'x_size': width_size , 'y_size': height_size}, 'POST', `/get-size-canvas/`);
--- static/js/join_points.js
+++ static/js/join_points.js
@@ -78,14 +78,14 @@ $("#canvasLines").click(function(e){
                     };
 
                     // Sent the data to the server
-                    connection_to_server(server_data, "POST", `0.0.0.0:${server_port}/game-data/`);
+                    connection_to_server(server_data, "POST", `/game-data/`);
 
                     // Get the game difficulty and redirect to the next game
-                    var difficulty_next_level = connection_to_server(server_data, "POST", `0.0.0.0:${server_port}/compute-game-dificulty/`);
+                    var difficulty_next_level = connection_to_server(server_data, "POST", `/compute-game-dificulty/`);
 
                     // Get the next level
                     if (confirm("Do you want to play the next level?")){
-                        window.location.replace(`0.0.0.0:${server_port}/game/` + difficulty_next_level);
+                        window.location.replace(`/game/` + difficulty_next_level);
                     };
                 };
             } else {
