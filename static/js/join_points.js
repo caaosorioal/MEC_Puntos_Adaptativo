5b05644a36615ef18e6cdea7e7f4b57f27e3cbf1
Carlos Andrés Osorio <caaosorioal@unal.edu.co>
2023-01-30 22:37:44 -0500

    Update requirements

--- static/js/join_points.js
+++ static/js/join_points.js
@@ -1,5 +1,5 @@
 // Get the port
-var server_port = process.env.YOUR_PORT || process.env.PORT || 8000;
+var server_port = process.env.PORT || 8000;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 server.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
